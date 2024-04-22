7b4e4ac1574aec6c0a67311ba3b814633e3d08f7
Vish <vishaylinparshotam@gmail.com>
2024-04-22 04:20:38 -0700

    error fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# Create a file
 file { '/tmp/school':
-  ensure  => 'file',
-  content => 'I love Puppet',
+  ensure  => 'file',   
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet',
 }
