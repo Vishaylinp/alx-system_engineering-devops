96160f69a744ae654b9375ac4fc8ea3d33013fb0
vishaylinp <vishaylinparshotam@gmail.com>
2024-05-24 05:25:22 -0700

    error fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,25 +11,14 @@ if __name__ == "__main__":
 
     response = requests.get(url)
 
-    n = response.json().get('name')
+    u_n = response.json().get('username')
 
     url_todo = url + "/todos"
     response = requests.get(url_todo)
     to_do_list = response.json()
 
-    i = 0
-    complete_t = []
-
-    for task in to_do_list:
-        if task.get('completed'):
-            complete_t.append(task)
-            i = i + 1
-
-    print(f"Employee {n} is done with tasks({i}/{len(to_do_list)}):")
-
-    for task in complete_t:
-        print("\t {}".format(task.get('title')))
-
     with open("{}.csv".format(worker_id), 'w') as f:
         for task in to_do_list:
-            f.write(f'"{n}","{task.get("completed")}","{task.get("title")}"\n')
+            f.write('"{}","{}","{}","{}"\n'
+                    .format(worker_id, u_n,
+                            task.get("completed"), task.get('title')))
