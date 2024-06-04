6188da9b7f046f0f532d724ea4fd9aa5c09a53ca
vishaylinp <vishaylinparshotam@gmail.com>
2024-06-04 04:54:38 -0700

    user-agent edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Number of sub"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "MyUniqueRedditApp/0.0.1"
-    }
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 \
+    Safari/537.36 Edg/125.0.0.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
