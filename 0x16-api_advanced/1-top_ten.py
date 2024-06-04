04fdcf1b60e88976d328d8fe42fe5121b821ba91
vishaylinp <vishaylinparshotam@gmail.com>
2024-06-04 05:47:08 -0700

    pycode

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Top ten"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers =  {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 \
     Safari/537.36 Edg/125.0.0.0"}
 
@@ -20,4 +20,4 @@ def top_ten(subreddit):
         print("None")
         return
     results = response.json().get("data")
-    [print(count.get("data").get("title")) for count in results.get("children")]
+    [print(i.get("data").get("title")) for i in results.get("children")]
