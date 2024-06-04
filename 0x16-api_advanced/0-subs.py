3d5bcba0e4e104e5d16a375a599e5be097d8381f
vishaylinp <vishaylinparshotam@gmail.com>
2024-06-04 05:16:33 -0700

    possible fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,15 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 \
     Safari/537.36 Edg/125.0.0.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+
+        data = response.json().get("data", {})
+        return data.get("subscribers", 0)
+    except requests.RequestException:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    except ValueError:
+        return 0
