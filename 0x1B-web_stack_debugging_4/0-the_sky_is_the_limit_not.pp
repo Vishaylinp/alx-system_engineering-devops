50951a6eb668510df4789984b95f00af80273809
Vish <vishaylinparshotam@gmail.com>
2024-06-11 10:32:03 -0700

    puppet

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Fix problem
+
+exec {'replacement':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restarting':
+  provider  => 'sudo service nginx restart',
+}
